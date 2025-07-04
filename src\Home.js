81feba3a92bfae0df3fb1ab69affad16583d70d4
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-04 23:21:24 +0500

    Add src\Home.js

---
+++ src\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Pakistan Atomic Power</h1>
+            <p>We are committed to generating clean and sustainable energy for the nation.</p>
+        </div>
+    );
+}
+
+export default Home;
